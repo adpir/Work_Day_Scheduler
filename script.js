7cb9e1ebb7027749fd073701b6adee4a80cd8595
Alice Piar <adpir30@gmail.com>
2020-10-28 12:41:12 -0500

    localStorage new updates

--- script.js
+++ script.js
@@ -3,4 +3,9 @@ $(".saveBtn").on("click", function(){
     var button= hourblock.split("-")[0]
     var plan= $("#" + button + "-txt").val()
     console.log(hourblock, plan)
+    localStorage.setItem(button,plan);
 });
+for (let i=7;i<=15;i++){
+    var userplan = localStorage.getItem(i);
+    $("#" + i + "-txt").val(userplan)
+}
