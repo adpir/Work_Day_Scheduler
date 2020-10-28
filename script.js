14c75c5edea987bc782f83f992d7f52a05d809b0
Alice Piar <adpir30@gmail.com>
2020-10-28 12:59:04 -0500

    new updates

--- script.js
+++ script.js
@@ -5,7 +5,19 @@ $(".saveBtn").on("click", function(){
     console.log(hourblock, plan)
     localStorage.setItem(button,plan);
 });
+var currentdate = moment()
+$("#currentDay").val(currentdate);
+var currentHour= moment().hours()
 for (let i=7;i<=15;i++){
     var userplan = localStorage.getItem(i);
     $("#" + i + "-txt").val(userplan)
+    if(currentHour <i){
+    $("#" + i + "-txt").addClass("future")
+    }
+    else if ( currentHour== i){
+   $("#" + i + "-txt").addClass("present")    
+    }
+    else {
+     $("#" + i + "-txt").addClass("past")
+    }
 }
