7c0e715e022e19e0e3645c7bf197f9c57056700c
Alice Piar <adpir30@gmail.com>
2020-10-28 17:45:53 -0500

    final commit

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 # Work_Day_Scheduler
+
+# Description
+This project's goal was to create a schedule calendar application that allows a user to save events for each hour of the day by modifying the starter code. This app will run in the browser and feature dynamically updated HTML and CSS powered by jQuery.
+We used  Moment.js library to work with date and time. 
+
+# Work Day Scheduler
+![image](./Assets/work_scheduler.jpeg)
+
+# Installation
+The installation I used is:
+* Terminal
+* Visual Studio Code
+* GitHub
+* GitLab
+* Moment - https://momentjs.com/ 
+
+
+# License
+
+2020 Alice Piar All rights Reserved.
+
+
+
--- script.js
+++ script.js
@@ -1,20 +1,21 @@
 $(".saveBtn").on("click", function(){
-    var hourblock = $(this).attr("id")
-    var button= hourblock.split("-")[0]
-    var plan= $("#" + button + "-txt").val()
+    let hourblock = $(this).attr("id")
+    let button= hourblock.split("-")[0]
+    let plan= $("#" + button + "-txt").val()
     console.log(hourblock, plan)
     localStorage.setItem(button,plan);
 });
-var currentdate = moment()
-$("#currentDay").val(currentdate);
-var currentHour= moment().hours()
-for (let i=7;i<=15;i++){
-    var userplan = localStorage.getItem(i);
+let currentdate = moment().format();
+console.log(currentdate)
+$("#currentDay").text(currentdate);
+let currentHour= moment().hours();
+for (let i=9;i<=17;i++){
+    let userplan = localStorage.getItem(i);
     $("#" + i + "-txt").val(userplan)
     if(currentHour <i){
     $("#" + i + "-txt").addClass("future")
     }
-    else if ( currentHour== i){
+    else if(currentHour == i){
    $("#" + i + "-txt").addClass("present")    
     }
     else {
